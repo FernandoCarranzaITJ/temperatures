40f36b668ccc13a9cc11c52519a5db44720377c3
FernandoCarranzaITJ <you@example.com>
2023-08-03 14:22:53 -0600

    Aplicacion de mediciones

--- mediciones/admin.py
+++ mediciones/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from mediciones.models import Area, Region, Measurements
 
 # Register your models here.
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ['name','owner','phone','country']
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['area','name']
+
+@admin.register(Measurements)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['region','created_at','temperature']
